787c75de63a3ea56b651ba27fd9de1b74e5d4096
tottoko123 <r6it7yi@gmail.com>
2022-05-27 06:33:58 +0000

    [more add] bookers1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,38 @@
 class BooksController < ApplicationController
-  def new
-  end
-
   def index
+    @books = Book.all
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/'
+  end
+  
+  private
+  
+  def book_params
+    params.permit(:title, :body)
+  end
+  
 end
